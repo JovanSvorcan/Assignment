73d362f2165b4d69977d01cb7dbd876ff71debf7
JovanSvorcan <jovan.svorcan@panonit.com>
2024-05-13 16:29:19 +0200

    Bugs fixed in TODO list management

--- src/UI/TodoManagmentViewModel.cs
+++ src/UI/TodoManagmentViewModel.cs
@@ -43,12 +43,17 @@ internal class TodoManagmentViewModel : Screen
         {
             _selectedItem = value;
             NotifyOfPropertyChange(() => SelectedItem);
+
+            IsDoneEnabled = SelectedItem == null ? true : !SelectedItem.Done;
+            NotifyOfPropertyChange(() => IsDoneEnabled);
         }
     }
 
     public ICommand AddTodoListCommand { get; private set; }
     public ICommand AddTodoItemCommand { get; private set; }
     public ICommand DoneTodoItemCommand { get; private set; }
+
+    public bool IsDoneEnabled { get; set; }
 
     public TodoManagmentViewModel(ISender sender, IWindowManager windowManager)
     {
@@ -82,16 +87,19 @@ internal class TodoManagmentViewModel : Screen
     {
         var todoList = new TodoListViewModel(_sender);
         await _windowManager.ShowDialogAsync(todoList);
+        await RefereshTodoLists();
     }
 
     private async void AddTodoItem(object obj)
     {
         var todoItem = new TodoItemViewModel(_sender, SelectedTodoList.Id);
         await _windowManager.ShowDialogAsync(todoItem);
+        await RefereshTodoLists();
     }
 
     private async void DoneTodoItem(object obj)
     {
         await _sender.Send(new DoneTodoItemCommand(SelectedItem.Id));
+        await RefereshTodoLists();
     }
 }
